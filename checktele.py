5e892b2590f774735435d38ab21471a4d6646cfc
Isugsh7 <165947173+Isugsh7@users.noreply.github.com>
2024-04-21 12:04:34 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -327,13 +327,13 @@ async def _(event):
                     with open("banned.txt", "a") as f:
                         f.write(f"\n{username}")
                 except Exception as eee:
-                    await Tepthon.(event.chat_id, f'''خطأ مع {username}
+                    await (event.chat_id, f'''خطأ مع {username}
     الخطأ :
     {str(eee)}''')
                     if "A wait of" in str(eee):
                         break
                     else:
-                        await Tepthon.(event.chat.id, " اجاك متاح !")
+                        await (event.chat.id, " اجاك متاح !")
             else:
                 pass
             trys += 1
